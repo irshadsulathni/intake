500fd982b144b7a0a7a6ebc36e0cf43d5e413ffa
irshadsulthani <irshadpallar2020@gmail.com>
2024-10-30 11:48:04 +0530

    second commit

--- objSumReduce.js
+++ objSumReduce.js
@@ -3,12 +3,6 @@ let arr = [{ab:5},{ab:10},[{ab:20}]]
 let flat = arr.flat().map((item) => item.ab)
 console.log(Math.min(...flat))
 
-
-
-
-
-
-
 let sum = arr.reduce((acc,val)=>{
     if (Array.isArray(val)) {
         return acc + val.reduce((subAcc, subVal) => subAcc + subVal.ab, 0);
@@ -16,5 +10,6 @@ let sum = arr.reduce((acc,val)=>{
     return acc + val.ab
 
 },0)
+
 
 console.log(sum)
