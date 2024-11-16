1980194b31318ca7867a1a57ed85d67f6594dba3
irshadsulthani <irshadpallar2020@gmail.com>
2024-11-16 12:27:51 +0530

    changes

--- practice.js
+++ practice.js
@@ -1,8 +1,8 @@
-function secondLargest(arr){
+function secondLargest(arr) {
     let max = arr[0]
     let sec = arr[0]
-    for(let i = 0; i < arr.length; i++){
-        if( arr[i] > max ){
+    for(let i = 0 ; i <arr.length; i++){
+        if(arr[i] > max){
             sec = max
             max = arr[i]
         }else if(arr[i] > sec && arr[i] < max){
@@ -25,6 +25,8 @@ let max = arr.reduce((acc,val)=>{
     }else if( val > second && val !== acc){
         second = val
     }
+
+    
     return acc
 })
 
